6e0a9317069c8a124795e53a564c291647086910
Surya Kristanto <suryakristanto1@gmail.com>
2023-03-16 15:22:52 +0700

    feat: :art: use dotenv

--- db.ts
+++ db.ts
@@ -1,10 +1,13 @@
 import * as mysql from 'mysql';
+import * as dotenv from 'dotenv';
+
+dotenv.config();
 
 export const pool = mysql.createPool({
-  host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'ecommerce',
-  port: 3306,
-  timezone: 'local',
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASS,
+  database: process.env.DB_NAME,
+  port: parseInt(process.env.DB_PORT),
+  timezone: process.env.TIMEZONE,
 });
