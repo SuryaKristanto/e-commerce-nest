7c216f30c715077a866ace8e6a08947ea7227007
Surya Kristanto <suryakristanto1@gmail.com>
2023-03-16 14:50:58 +0700

    docs: :art: add needed folder and file

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,9 +2,12 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ProductModule } from './product/product.module';
+import { AuthModule } from './auth/auth.module';
+import { OrderModule } from './order/order.module';
+import { WishlistModule } from './wishlist/wishlist.module';
 
 @Module({
-  imports: [ProductModule],
+  imports: [ProductModule, AuthModule, OrderModule, WishlistModule],
   controllers: [AppController],
   providers: [AppService],
 })
